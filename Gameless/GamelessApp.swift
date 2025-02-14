0a09454814e847b7aa99646332438a135cfeb293
Mark Mauro <markymauro@gmail.com>
2025-02-14 14:24:02 -0500

    Initial Commit

---
+++ Gameless/GamelessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamelessApp.swift
+//  Gameless
+//
+//  Created by Mark Mauro on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GamelessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GamelessTests/GamelessTests.swift
@@ -0,0 +1,17 @@
+//
+//  GamelessTests.swift
+//  GamelessTests
+//
+//  Created by Mark Mauro on 2/14/25.
+//
+
+import Testing
+@testable import Gameless
+
+struct GamelessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
